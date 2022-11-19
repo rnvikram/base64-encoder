a00f2745c8bf99c963d79fec904625ef9a5fb4ad
Vikram Niranjen <vikramniranjen@gmail.com>
2022-11-20 00:01:07 +1000

    all base64 package

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ import base64
 
 st.title("Base64 Encoder")
 
+# st.header("Text to encode")
 text_to_encode = st.text_area("Text to encode", value="")
 
 ascii_encoded = text_to_encode.encode("ascii")
 
 base64_encoded = base64.b64encode(ascii_encoded).decode("ascii")
 
-st.text_area("Base64 encoded output", value=base64_encoded, disabled=True)
+st.header("Base64 encoded output")
+st.text_area(value=base64_encoded, disabled=True)
