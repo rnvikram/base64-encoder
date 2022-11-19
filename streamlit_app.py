fe2b7cf9e9fc33dec40e06192f20a5733ee4416b
Vikram Niranjen <vikramniranjen@gmail.com>
2022-11-19 23:41:58 +1000

    basic app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 """Hello world"""
+
+
+
+text_to_encode = st.text_area("Text to encode", value="")
+
+ascii_encoded = text_to_encode.encode("ascii")
+
+base64.b64encode(ascii_encoded)
